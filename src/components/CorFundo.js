fddf1807cc408d959f96ea8a813a200caebb55d2
Elizabethcristina10 <elizabethcristina2908@gmail.com>
2023-12-21 00:00:24 -0300

    continuando

---
+++ src/components/CorFundo.js
@@ -0,0 +1,13 @@
+import styles from "./Cor.module.css"
+
+
+function CorFundo() {
+     return (
+       <div className={styles.CorFundo}>
+        <h1> Começando o codigo!</h1>
+       </div>
+     );
+   }
+
+   export default CorFundo
+   
